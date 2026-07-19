8dc36645a224c136fe252bde9e94fa17844dde69
KrishnaSSH <krishna.pytech@gmail.com>
2026-07-20 00:44:13 +0530

    update wpctl status, refactor pipewire script

---
+++ .local/scripts/start-pipewire.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# https://github.com/krishnassh/dotfiles
+# Author: KrishnaSSH
+# LICENSE: CC0 1.0 Universal
+# You may get a copy of this license from https://creativecommons.org/publicdomain/zero/1.0/legalcode.en
+
+pipewire
+wireplumber
+sleep 1
+pipewire-pulse
+
